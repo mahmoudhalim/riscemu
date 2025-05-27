705ef0eb0f008c7a6eee0748d72a5d8ff10fe93f
mahmoud isa <mahmoudahalim0@gmail.com>
2025-05-27 05:02:58 +0300

    refactor: Update Funct3 enumeration and simplify funct7 type in Rinstruction

--- instructions.h
+++ instructions.h
@@ -6,15 +6,19 @@
 typedef enum class OpCode : uint8_t { Rtype = 0x33 } OpCode;
 typedef enum class Funct3 : uint8_t {
   addSub = 0x0,
+  sll = 0x1,
+  slt = 0x2,
+  sltu = 0x3,
+  xorr = 0x4,
+  sr = 0x5,
+  orr = 0x6,
+  andd = 0x7
 } Funct3;
-typedef enum class Funct7 : uint8_t {
-  add = 0b0000000,
-  sub = 0b0100000,
-} Funct7;
+
 
 struct Rinstruction {
   uint32_t raw;
-  Funct7 funct7;
+  uint8_t funct7;
   uint8_t rs2;
   uint8_t rs1;
   Funct3 funct3;
@@ -23,7 +27,7 @@ struct Rinstruction {
 
   Rinstruction(uint32_t raw) : raw(raw) { decode(); }
   void decode() {
-    funct7 = static_cast<Funct7>(raw >> 25 & 0b1111111);
+    funct7 = raw >> 25 & 0b1111111;
     rs2 = (raw >> 20) & 0b11111;
     rs1 = (raw >> 15) & 0b11111;
     funct3 = static_cast<Funct3>((raw >> 12) & 0b111);
