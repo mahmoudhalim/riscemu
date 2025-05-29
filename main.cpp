6661d357a1b2803d1675a983f916c7470d17157c
mahmoud isa <mahmoudahalim0@gmail.com>
2025-05-30 02:57:29 +0300

    fix: correct CPU instantiation and instruction execution in main function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <cstdint>
 
 int main() {
-  cpu c;
-  uint32_t IM[] = {0x007302b3};
-  auto instr = Rinstruction(IM[0]);
-  c.exec(instr);
+  CPU c;
+  uint32_t IM[] = {0x007312b3};
+  c.execute(IM[0]);
   // instr.print();
   c.print();
   return 0;
